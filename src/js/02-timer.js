245b5ae16785f6f0cdcaa268629fef6587e8bde6
SunAndrea <andriishtohryn@gmail.com>
2022-12-25 19:41:32 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,25 +1,33 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
-const timerEl = document.querySelector(`#datetime-picker`);
 const btnEl = document.querySelector(`[data-start]`);
+const daysEl = document.querySelector(`[data-days]`);
+const hoursEl = document.querySelector(`[data-hours]`);
+const minutesEl = document.querySelector(`[data-minutes]`);
+const secondsEl = document.querySelector(`[data-seconds]`);
+
 let pickedTime = null;
+let timeDifference = null;
+let intervalId = null;
 
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
+
   onClose(selectedDates) {
     pickedTime = selectedDates[0];
+
     if (selectedDates[0] <= Date.now()) {
       window.alert(`Please choose a date in the future`);
       btnEl.disabled = true;
     }
   },
 };
-// console.log(pickedTime);
 
 flatpickr(`#datetime-picker`, options);
+
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
@@ -36,6 +44,27 @@ function convertMs(ms) {
   // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
-  return { days, hours, minutes, seconds };
+  return (dataForTimer = { days, hours, minutes, seconds });
 }
-btnEl.addEventListener(`click`, convertMs);
+
+function onTimerStarter(evt) {
+  intervalId = setInterval(() => {
+    timeDifference = pickedTime.getTime() - Date.now();
+
+    if (timeDifference <= 0) {
+      clearInterval(timeDifference);
+      return;
+    }
+    convertMs(timeDifference);
+    daysEl.textContent = addLeadingZero(dataForTimer.days);
+    hoursEl.textContent = addLeadingZero(dataForTimer.hours);
+    minutesEl.textContent = addLeadingZero(dataForTimer.minutes);
+    secondsEl.textContent = addLeadingZero(dataForTimer.seconds);
+  }, 1000);
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, `0`);
+}
+
+btnEl.addEventListener(`click`, onTimerStarter);
